3d9f19b34b0107d29f8e33d34eb52a4f7cccd308
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-09-10 13:27:38 +0500

    changes

---
+++ Lecture-22/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+      "medium-turquoise":"#48D1CC",
+      "mypink":"#FF69B4",
+    },
+    fontFamily: {
+      serif: ['Merriweather', 'serif'],
+    },
+      fontSize: {
+        "10xl":"1rem"
+      },
+    },
+  },
+  plugins: [],
+}
+
