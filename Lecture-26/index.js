8e95705a63c70e0053d143545e1e0b450a387424
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-09-30 12:50:49 +0500

    changes

---
+++ Lecture-26/index.js
@@ -0,0 +1,163 @@
+// ************ Data types ****************
+
+// let number = 30;
+// console.log(typeof(number));  //it shows number
+
+// let string = "Mohsin Raza";
+// console.log(typeof(string));  //it shows string
+
+// let bool = true; // or false
+// console.log(typeof(bool));  //it shows boolean
+
+// let a;
+// console.log(a);
+// console.log(typeof(a));  //it shows undefined
+
+// let b = null;
+// console.log(b);
+// console.log(typeof(b));  //it shows object
+
+// let id1 = Symbol('id');
+// console.log(id1);
+
+// let id2 = Symbol('mohsin');
+// console.log(id2);
+
+// console.log(id1 === id2); //it shows false becase symbol make unique ids.
+
+// let big = 123456789012345678901234567890n;
+// console.log(big + 2n); // 123456789012345678901234567892n
+
+// let student = {
+//     full_Name : "Mohsin Raz",
+//     Roll_Number : 234567,
+//     Program : "bscs",
+//     depart : "computer science",
+// }
+// console.log(student);
+
+//************* Operators ***************/
+
+//********* Arithematic operator **************/
+
+// let num1 = 5;
+// let num2 = 10;
+// let sum = num1 + num2;
+// console.log(sum);
+
+// let num1 = 5;
+// let num2 = 10;
+// let sub = num1 - num2;
+// console.log(sub);
+
+// let num1 = 5;
+// let num2 = 10;
+// let mul = num1 * num2;
+// console.log(mul);
+
+// let num1 = 5;
+// let num2 = 10;
+// let divide = num1 / num2;
+// console.log(divide);
+
+// let num1 = 5;
+// let num2 = 10;
+// let modulas = num1 % num2;
+// console.log(modulas);
+
+// let pow = 2 ** 15;
+// console.log(pow);
+
+//********* Assigment operator **************/
+
+// let number = 10;
+//     number += 5;  // number = number + 5;
+//     console.log(number);
+    
+// let number = 10;
+//     number -= 5;
+//     console.log(number);
+
+// let number = 10;
+//     number *= 5;
+//     console.log(number);
+
+// let number = 10;
+//     number **= 5;
+//     console.log(number);
+
+// let number = 10;
+//     number /= 5;
+//     console.log(number);
+
+// let number = 10;
+//     number %= 5;
+//     console.log(number);
+
+
+// ******************* Unary operator*****************
+
+// let count = 4;
+//   console.log(count++);
+//   console.log(count); 
+  
+//   count = 4;
+//   console.log(count--);
+//   console.log(count);
+
+//   count = 4;
+//   console.log(++count);
+//   console.log(count);
+
+//   count = 4;
+//   console.log(--count);
+//   console.log(count);
+
+// *************** Logical and **************
+
+// console.log(2 && 0);
+// console.log(0 && 2);
+// console.log(2 && 2);
+// console.log(1 && 1);
+
+// *************** Logical OR **************
+
+
+// console.log(2 || 0);
+// console.log(0 || 2);
+// console.log(2 || 2);
+// console.log(1 || 1);
+
+
+// **************comparison operator ****************
+
+let a = 5;
+let b = 10;
+
+console.log(a > b);
+console.log(a < b);
+console.log(a <= b);
+console.log(a >= b);
+console.log(a != b);
+console.log(a !== b);
+console.log(a == b);
+console.log(a === b);
+
+console.log(1 + '1');// it shows 11 not 2
+
+let c;
+console.log(typeof(typeof(c)));
+
+
+
+   
+
+
+
+
+
+
+
+
+
+  
