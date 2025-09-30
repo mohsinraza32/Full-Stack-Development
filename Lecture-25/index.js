9fb6ed0f3bc1cc070d05dd4211f6f6df8e8b350e
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-09-30 09:41:08 +0500

    changes

---
+++ Lecture-25/index.js
@@ -0,0 +1,21 @@
+const idNo = 4260403992109;
+console.log("Your id No is:", idNo);  //global scope
+
+{
+  const idNo = 4260403992109;     // local scope
+  console.log("Your id No is:", idNo);
+
+//   const idNo = 426040399210988; 
+//   console.log("Your id No is:", idNo);
+}
+
+let myName = "Mohsin Raza";   //global scope
+console.log(myName);
+
+{
+  let myName = "Mohsin Raza";  //local scope
+  console.log(myName);
+
+//   let myName = "Mohsin Raza";
+//   console.log(myName);
+}
