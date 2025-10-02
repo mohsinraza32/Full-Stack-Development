8aa8d85e489a97370c4abceb859533427530cebf
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-10-02 13:52:04 +0500

    changes

---
+++ Lecture-28/index.js
@@ -0,0 +1,105 @@
+// const mohsin_urdu = 60;
+// const mohsin_english = 70;
+// const mohsin_math = 80;
+
+// let total = mohsin_urdu + mohsin_english + mohsin_math;
+// console.log('Total Number: ', total);
+
+
+// function totalNumber(val1,val2,val3){
+//   let sum = val1 + val2 + val3;
+//   console.log("Total sum:",sum);  
+// }
+// totalNumber(50,60,90);
+
+// function totalNumber(urdu,english,math){
+//   let sum = urdu + english + math;
+//   console.log("Total sum:",sum);  
+// }
+// totalNumber(50,60,90);
+
+// function totalNumber(urdu,english,math){
+//   let sum = urdu + english + math;
+//   return sum;
+// }
+// let result1 = totalNumber(80,60,60);
+// console.log("Mohsin_Marks:",result1);
+// let result2 = totalNumber(60,50,70);
+// console.log("Husnain_Marks:",result2);
+// let result3 = totalNumber(40,80,50);
+// console.log("Zulfi_Marks:",result3);
+// let result4 = totalNumber(50,50,90);
+// console.log("Babu_Marks:",result4);
+
+// let num = 52.34567;
+// let result = num.toFixed(4);
+// console.log(result);
+
+// let num = 52.34567;
+// let result = num.toPrecision(3);
+// console.log(result);
+
+// let number = new Number(100);
+
+// console.log(number);
+// console.log(typeof number);
+// let numToString = (number.toString());
+// console.log(numToString);
+// console.log(typeof numToString);
+
+
+let str = '  hello mohsin  ';
+
+// console.log(str.replace('el','ol'));
+// console.log(str.split('o'));
+// console.log(str.length);
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase());
+// console.log(str.match('mohsin'));
+// console.log(str.charAt(6));
+// console.log(str.indexOf('s'));
+// console.log(str.charCodeAt(2));
+// console.log(str.codePointAt(2));
+// console.log(str.concat(' raza'));
+// console.log(str.at(2));
+// console.log(str[0]);
+// console.log(str.slice(4,9));
+// console.log(str.substring(0,-3));
+// console.log(str.isWellFormed());
+// console.log(str.toWellFormed());
+// console.log(str.trim());
+// console.log(str.trimStart());
+// console.log(str.trimEnd());
+
+
+// let text = '5';
+// console.log(text.padStart(4,'0'));
+// console.log(text.padEnd(4,'0'));
+// console.log(text.repeat(5));
+
+
+let text = 'I love cats. cats are very easy to love. cats are very popular';
+console.log(text.replaceAll("cats","dogs"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+ 
