4c1619d474964b78e1041e62450197fb301d4ebe
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-10-03 12:53:15 +0500

    changes

---
+++ Lecture-29/index.js
@@ -0,0 +1,75 @@
+// let arr = [2,45,66,77,88,65];
+// console.log(arr);
+// console.log(arr[3]);
+// console.log(arr[2]);
+
+// let arr = ['mohsin','abid','zulfi','asif'];
+// console.log(arr);
+// console.log(arr[3]);
+// console.log(arr[2]);
+
+// let arr = [2,'mohsin',true,77,'ali',65];
+// console.log(arr);
+// console.log(arr[3]);
+// console.log(arr[2]);
+
+let arr = [2,45,66,77,88,65];
+//    arr.push(4,6,7);
+//    arr.pop();
+// console.log(arr);
+
+// let newArr = arr.concat(23,55,66,99);
+// console.log(arr);
+// console.log(newArr);
+
+// let findIndex = arr.indexOf(65);
+// console.log(findIndex);
+
+// let join = arr.join('*');
+// console.log(join);
+// console.log(arr.length);
+
+// arr.reverse();
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// arr.unshift(3,5,6);
+// console.log(arr);
+
+// let arr1 = [2,3,5,6,7,8,5,5,4];
+//  let sliceArr = arr1.slice(4,7);
+// console.log(sliceArr);
+
+// let arr1 = [2,3,5,6,7,8,5,5,4];
+//  let spliceArr = arr1.splice(4,7);
+// console.log(spliceArr);
+
+// let arr1 = [1,2,3,4,5];
+// let spliced = arr1.splice(1,3); 
+// console.log(spliced); // [2,3,4]
+// console.log(arr1);     // [1,5] (original badal gaya)
+
+// let array = [1,2,3,4,5,6,7,8,9];
+
+// for(let i = 0; i < arr.length; i++){
+//     console.log(array[i]);
+// }
+
+// ********Array Destructuring**********
+
+let arr1 = [4,6,77,7,4,3];
+let [first,second,third,fourth,fifth,six] = arr1;
+console.log(first);
+console.log(fourth);
+
+
+
+
+
+
+
+
+
+
