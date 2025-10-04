82e15a8af0fec3b6e7780a2d7479b73e1e7b9874
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-10-04 16:43:05 +0500

    changes

---
+++ Lecture-30/index.js
@@ -0,0 +1,16 @@
+let arr = [32,34,99,44,7];
+
+// arr.forEach(function (val) {
+//     val += 4;
+//     console.log(val);
+    
+// })
+
+// const hello = (arr) => {
+// console.log(arr);
+
+// }
+// hello(arr);
+
+arr.forEach((val) => console.log(val)
+)
