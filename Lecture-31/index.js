f17a54424e89d057b9f41c000a3a106ebd1ed03b
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-10-05 12:27:26 +0500

    changes

---
+++ Lecture-31/index.js
@@ -0,0 +1,73 @@
+
+
+// {
+//     let name = 'mohsin';
+//     console.log(name);
+// }
+
+// {
+//     let name = 'mohsin';
+    
+// }
+// console.log(name); 
+// {
+//     const name = 'mohsin';
+// }
+// console.log(name);
+
+// let name = "mohsin";
+// {
+//   console.log(name);
+// }
+
+// const name = "mohsin";
+// {
+//   console.log(name);
+// }
+
+
+// {
+//   console.log(name);
+//   let name = "mohsin";
+// }
+
+// let name = 'mohsin';
+
+// for(let i = 0; i <= 5; i++){
+
+//     console.log(name);
+    
+// }
+
+// ***********function scope************
+
+// let name = "mohsin";
+
+// function hello(){
+//     console.log(name);
+    
+// }
+// hello();
+
+
+// function hello(){
+//     let name = "mohsin";
+// }
+// hello();
+// console.log(name);
+
+function hello1(){
+
+    let myName = 'mohsin';
+    console.log('kia hal h bhai!');
+
+    function hello2(){
+      
+        let myAge = 12;
+        console.log(myAge);  
+        console.log(myName);
+    }  
+    
+    hello2();
+}
+hello1();
