2b1d6f1b96e4673d04ed4fcb13ff0f8f121e7fe0
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-10-06 17:01:31 +0500

    changes

---
+++ Project-8/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        "custom-black":"#0c1c33",
+        "custom-blue":"#2a5ddb",
+        "custom-white":"#ffffff"
+      },
+      height:{
+        "22":"5.5rem"
+      }
+    },
+  },
+  plugins: [],
+}
