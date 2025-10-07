4afe7f1b4417f9ad79169592fbddb1865776eedc
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-10-07 11:42:32 +0500

    changes

---
+++ Lecture-33/index.js
@@ -0,0 +1,28 @@
+// var num1 = 2;
+// var num2 = 5;
+
+// function add(a,b){
+//     var total = a + b;
+//     return total;
+// }
+
+// var sum = add(num1,num2);
+// console.log(sum);
+
+function one(){
+    var a = 'hello1';
+    console.log('inside one');
+
+function two(){
+    let b = 'hello2';
+    console.log('inside two');
+
+function three(){
+    let c = 'hello3';
+    console.log('inside three');
+}
+three();    
+}
+two();    
+}
+one();
