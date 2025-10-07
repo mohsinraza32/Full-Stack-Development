2a5bdd8c968f93844a32e52211f69afd676a90eb
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-10-07 18:04:39 +0500

    changes

--- Project-8/tailwind.config.js
+++ Project-8/tailwind.config.js
@@ -6,11 +6,20 @@ module.exports = {
       colors:{
         "custom-black":"#0c1c33",
         "custom-blue":"#2a5ddb",
-        "custom-white":"#ffffff"
+        "custom-white":"#ffffff",
+        "custom-gray":"#515151",
+        "custom-blue":"#0c1c33",
+        "custom-white-shade":"#f4f1e1",
+        "custom-new-blue":"#95b2db"
       },
       height:{
         "22":"5.5rem"
-      }
+      },
+      screens:{
+        'vvsmall':{'max':'520px'},
+        'vsmall':{'max':'750px'},
+        'small':{'max':'1200px'} 
+      },
     },
   },
   plugins: [],
