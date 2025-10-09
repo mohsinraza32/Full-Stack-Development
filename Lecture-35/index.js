9b5b5bb926ee6f2928c0909d80e25acab79232be
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-10-09 12:12:28 +0500

    changes

---
+++ Lecture-35/index.js
@@ -0,0 +1,32 @@
+//implicit
+// let a = 2;  
+// let b = '4';
+// let c = a + b;
+
+// console.log(typeof c);
+
+//explicit
+// let a = 2;  //explicit
+// let b = Number('4');
+// let c = a + b;
+
+// console.log(typeof c);
+
+// console.log(7 + '50');  // 750
+// console.log(7 + 6 + '50'); // 1350
+// console.log(7 + '50' + 4); //7504
+// console.log(7 + '50' + 3 + 2 +1); //750321
+// console.log(7 + '50' + (3 + 3 + 1)); //7507
+//console.log(7 + null); // 7
+//console.log(7 + true); // 1 qke ke true = 1
+//console.log(7 - '50'); //43 qke subtraction meye as a number ki trah treat krta h not string.
+//console.log('ali' - 50); //NaN
+//console.log(7 - 'ali');  // NaN
+//console.log({} - 10);  // NaN
+console.log([] - 10); //-10
+
+
+
+
+
+
