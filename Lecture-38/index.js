fc41b676685422690d24d02c3eb2278909d419d9
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-10-13 16:54:24 +0500

    changes

--- Lecture-38/index.js
+++ Lecture-38/index.js
@@ -1,5 +1,5 @@
-let child1 = document.getElementById('child1');
-let maindiv = document.getElementById('main-div');
+// let child1 = document.getElementById('child1');
+// let maindiv = document.getElementById('main-div');
 
 // event bubbling
 // maindiv.addEventListener('click' , () => {
@@ -20,3 +20,29 @@ let maindiv = document.getElementById('main-div');
 //     console.log('child clicked');
     
 // },true)
+
+//Building mini project Drag and Drop
+
+let box1 = document.querySelector('.box-1');
+let box2 = document.querySelector('.box-2');
+let items = document.getElementsByClassName('item');
+
+for(let item of items){
+    // item.addEventListener('dragstart', () => {
+
+    // })
+    // item.addEventListener('dragenter', () => {
+
+    // })
+    // item.addEventListener('dragover', () => {
+
+    // })
+    // item.addEventListener('dragleave', () => {
+
+    // })
+    // item.addEventListener('dragend', () => {
+
+    // })
+    
+}
+
