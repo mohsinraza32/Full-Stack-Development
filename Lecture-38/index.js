1083fd83243e49935feba36c40db906bf2419f86
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-10-14 11:30:54 +0500

    changes

--- Lecture-38/index.js
+++ Lecture-38/index.js
@@ -3,22 +3,22 @@
 
 // event bubbling
 // maindiv.addEventListener('click' , () => {
-//     console.log('parent clicked'); 
+//     console.log('parent clicked');
 
 // })
 // child1.addEventListener('click' , () => {
 //     console.log('child clicked');
-    
+
 // })
 
 //event capturing
 // maindiv.addEventListener('click' , () => {
-//     console.log('parent clicked'); 
+//     console.log('parent clicked');
 
 // },true)
 // child1.addEventListener('click' , () => {
 //     console.log('child clicked');
-    
+
 // },true)
 
 //Building mini project Drag and Drop
@@ -28,21 +28,27 @@ let box2 = document.querySelector('.box-2');
 let items = document.getElementsByClassName('item');
 
 for(let item of items){
-    // item.addEventListener('dragstart', () => {
-
-    // })
-    // item.addEventListener('dragenter', () => {
-
-    // })
-    // item.addEventListener('dragover', () => {
-
-    // })
-    // item.addEventListener('dragleave', () => {
-
-    // })
-    // item.addEventListener('dragend', () => {
-
-    // })
-    
+    item.addEventListener('dragstart', (e) => {
+        //  console.log(e.target);
+        let data = e.target;
+        // console.log(data);
+        box2.addEventListener('dragover', (e) => {
+           e.preventDefault();
+           e.stopPropagation();
+        })
+        box2.addEventListener('drop', () => {
+            box2.append(data);
+            data = '';
+        })
+        box1.addEventListener('dragover', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+        })
+        box1.addEventListener('drop', () => {
+            box1.append(data);
+            data = '';
+        })
+        
+         
+    })
 }
-
