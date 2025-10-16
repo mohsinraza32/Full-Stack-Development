e181e0b752a7a9a8e9c01029b4af56ecc56ccef1
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-10-16 13:28:05 +0500

    changes

---
+++ Project-10/index.js
@@ -0,0 +1,40 @@
+let ticTac = document.querySelector(".ticTac");
+
+let currentPlayer = "X";
+
+let winningCondition = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,5,8],
+]
+ticTac.addEventListener("click", (e) => {
+  //    if(currentPlayer === 'X'){
+  //       currentPlayer = 'O';
+  //    }else{
+  //      currentPlayer = 'X';
+  //    }
+  if (e.target.className !== "ticTac") {
+    if (e.target.innerText === "") {
+      e.target.textContent = currentPlayer;
+      winner();
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+    }
+  }
+
+  function winner(){
+   winningCondition.forEach((items) => {
+      let index0 = items[0];
+      let index1 = items[1];
+      let index2 = items[2];
+
+      console.log(index0, index1, index2);
+      
+   })
+    
+  }
+});
