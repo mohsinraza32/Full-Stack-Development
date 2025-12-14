7dcb6e30383f1000efcef8e79c1f41873a74a11b
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-12-14 12:26:00 +0500

    changes

--- Lecture-42/index.js
+++ Lecture-42/index.js
@@ -55,58 +55,58 @@
 //     })
 // })
 
+//upper code solve inversion of control problem but with promise hell.
 
+//Now we solve promise hell from our code
 
 function searchFood(item){
-    return new Promise(function fun1(res, rej){
-        console.log('Searching start for',item, '............');
-        setTimeout(function timer1(){
-            let data = `List of ${item}`;
-            res({item, data});
-
-        },3000)        
-    })
+   return new Promise(function fun1(res, rej){
+     console.log('Searching start for', item, '........');
+     setTimeout(function timer1(){
+        let data = `List of ${item}`;
+        //res(item, data);   wrong
+        // res([item,data]);  correct 
+        res({item,data}); 
+     },3000);
+     
+   });
 }
 
 function orderFood(item){
-  return new Promise(function fun2(res, rej){
-      console.log("Select", item);
-        setTimeout(function timer2(){
-         let id = Math.floor(Math.random() * 999999);
-         res({item,id});
-      },4000)
-  })
-}
-
-function payment(item, id){
-    return new Promise(function fun3(res, rej){
-        console.log("Payment starting for orderId no.", id);
-        setTimeout(function timer3(){
-            let status = true;
-            res(status);
-        },5000)
+    return new Promise(function fun2(res, rej){
+        console.log("Select", item);
+        setTimeout(function timer2() {
+          let id = Math.floor(Math.random() * 999999);
+          res({item,id});
+        }, 4000);
     })
+  
 }
 
-let result = searchFood('chicken biryani')
+function payment(item,id){
+
+    return new Promise(function fun3(res, rej){
+        console.log(`payment started for ${item} with id no.`, id);
+        setTimeout(function timer3() {
+           let status = true;
+           res(status); 
+        }, 6000);
+    })
+    
+}
+
+let result = searchFood('biryani')
 .then(function success1(detail){
     console.log(detail.data);
     return orderFood(detail.item);
 })
 .then(function success2(detail2){
    console.log("Orderid created successfully with id no.", detail2.id);
-    return payment(detail2.item, detail2.id);
+   return payment(detail2.item, detail2.id);
+   
 })
 .then(function success3(status){
-    console.log("Payment successfull with status,", status);
+    console.log(`payment successfully with status`, status);
 })
- 
-    
-
-
-
-
-
-
 
 
