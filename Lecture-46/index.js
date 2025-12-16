48eeaf2e76151638a8e911bd46932f8ce270cae1
Mohsin Raza <mr.mohsinraza32@gmail.com>
2025-12-16 16:32:12 +0500

    changes

--- Lecture-46/index.js
+++ Lecture-46/index.js
@@ -19,10 +19,11 @@
 // }
 // api();
 
-
+let num1 = 5;
+let num2 = 10;
 async function api(){
     try{
-        let response = await fetch("https://jsonplaceholder.typicode.com/posts");
+        let response = await fetch(`https://jsonplaceholder.typicode.com/sum?a=${num1}&b=${num2}`);
         let result = await response.json();
         console.log(result);
     }catch(err){
